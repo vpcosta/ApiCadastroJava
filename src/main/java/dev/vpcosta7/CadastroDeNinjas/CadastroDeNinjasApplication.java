485ba92359edc87bc6a3bd226fb9606c16cafe4c
vpcosta <vpcosta258@gmail.com>
2025-07-25 09:11:29 -0300

    Configuração inicial do projeto

---
+++ src/main/java/dev/vpcosta7/CadastroDeNinjas/CadastroDeNinjasApplication.java
@@ -0,0 +1,13 @@
+package dev.vpcosta7.CadastroDeNinjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeNinjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeNinjasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/vpcosta7/CadastroDeNinjas/CadastroDeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vpcosta7.CadastroDeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
