f662e2bd4c25f3708ea93c639a2f96025905739c
vpcosta <vpcosta258@gmail.com>
2025-07-28 16:12:26 -0300

    Criação da entidade para cadastro no banco dedados

--- src/main/java/dev/vpcosta7/CadastroDeNinjas/NinjaController.java
+++ src/main/java/dev/vpcosta7/CadastroDeNinjas/NinjaController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class Controller {
+public class NinjaController {
 
     @GetMapping("/boas-vindas")
     public String boasVindas() {
